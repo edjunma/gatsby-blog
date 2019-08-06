c721f12880c00ead47104c6508696b575ba3d326
edjunma <junwenma@buffalo.edu>
2019-08-06 01:33:58 -0400

    finish blog title and link components, setup blog, post to netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => {
   console.log(data)
@@ -15,9 +25,11 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount}</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}{" "}
-            </span>
+            <BlogLink to={node.fields.slug}>
+              <span>
+                {node.frontmatter.title} - {node.frontmatter.date}{" "}
+              </span>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))}
@@ -28,7 +40,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount {
         edges {
           node {
@@ -38,7 +50,9 @@ export const query = graphql`
               title
               date
             }
-            excerpt
+            fields {
+              slug
+            }
           }
         }
       }
